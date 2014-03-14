2bbbd782d17364776cf92f3a19e36419b1a80b22
Apon Palanuwech <palaa159@newschool.edu>
2014-03-14 09:59:35 -0400

    update

--- platforms/android/bin/HelloCordova-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/aponpalanuwech/Dropbox/_RECENTWORKS/2014_Spring/MOTF/motf_final/platforms/android/bin/HelloCordova-debug-unaligned.apk : \
-/Users/aponpalanuwech/Dropbox/_RECENTWORKS/2014_Spring/MOTF/motf_final/platforms/android/bin/HelloCordova.ap_ \
-/Users/aponpalanuwech/Dropbox/_RECENTWORKS/2014_Spring/MOTF/motf_final/platforms/android/bin/classes.dex \
-/Users/aponpalanuwech/Dropbox/_RECENTWORKS/2014_Spring/MOTF/motf_final/platforms/android/CordovaLib/bin/classes.jar \
